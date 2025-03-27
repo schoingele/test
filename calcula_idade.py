950de05cc9c4cc325560fa9f7e9e5e2197964a00
RODRIGO SCHOINGELE DA SILVEIRA <85651885072@senacrs.edu.br>
2025-03-27 19:48:25 -0300

    calcula_idade

---
+++ calcula_idade.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+data_nasc = input("Digite sua data de nacsimento (dd/mm/yyyy): ")
+nascimento = datetime.strptime(data_nasc, %d/%m/%y)
+hoje = datetime.today()
+idade = hoje.year - nascimento.year
+
+print(f"voce tem {idade} anos.")
